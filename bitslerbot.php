c406e88a4b7bb0dbec3bda176474fc61f7638c20
Dwi Siswanto <dwisiswant0@users.noreply.github.com>
2017-08-27 08:15:14 +0700

    Update bitslerbot.php

--- bitslerbot.php
+++ bitslerbot.php
@@ -6,22 +6,27 @@ $_SESSION['amount'] = 0;
 $_SESSION['lose'] = 0;
 $config = include "bitslerbot.config.php";
 $bitslerbot = new BitslerBOT\ngepet($config['token'], $config['uname']);
+// $balance = $bitslerbot->getbalance();
 
-echo "\n  # BitslerBOT v1.0 (Beta)";
+#############################
+echo "\n  # BitslerBOT v1.1";
 echo "\n  # @ 2017, dw1\n\n";
+#############################
 
 while (true) {
-	$amount = ($_SESSION['amount'] == 0 ? ($config['amount']*0.00000001) : $_SESSION['amount']);
-	$bet = $bitslerbot->caraycruz($amount, $config['choose']);
+	$amount = ($_SESSION['amount'] == 0 ? ($config['amount'] * 0.00000001) : $_SESSION['amount']);
+	$type = (int) $config['type'];
+	$bet = ($type == 1 ? $bitslerbot->roll_dice($amount, $config['condition'], $config['game']) : ($type == 2 ? $bitslerbot->caraycruz($amount, $config['choose']) : exit("You've not configured the bet type or incorrect.\n")));
 
-	if ($bet['status'] !== 1) exit($bet['error'] . "\n");
-	echo (OS ? "\x1b[2;3" . ($bet['return'] == 1 ? "2" : "1") . ";7;47m" : null) . "[" . $bet['time'] . "] " . ($bet['return'] == 1 ? "WIN!" : "LOSE!") . " RESULT " . $bet['result_coins'] . " | CHOOSE " . $bet['choose_coins'] . " | BET: " . $bet['bet'] . " | BALANCE: " . $bet['balance'] . (OS ? "\x1b[0m" : null) . "\n";
+	if ($bet['success'] !== "true") exit($bet['value'] . "\n");
+	$status = strpos($bet['amount_return'], "-");
+	echo (OS ? "\x1b[2;3" . ($status === false ? "2" : "1") . ";7;47m" : null) . "[" . $bet['time'] . "] " . ($status === false ? "WIN!" : "LOSE!") . " " . ($type == 1 ? "ROLL " . $bet['roll_number'] . " | GAME " . $bet['condition'] . $bet['game'] : ($type == 2 ? "RESULT " . $bet['result_coins'] . " | CHOOSE " . $bet['choose_coins'] : exit())) . " | BET: " . $bet['amount'] . " | BALANCE: " . $bet['new_balance'] . (OS ? "\x1b[0m" : null) . "\n";
 
-	if ($bet['return'] == 1) {
+	if ($status === false) {
 		$_SESSION['amount'] = 0;
 		$_SESSION['lose'] = 0;
 	} else {
-		$_SESSION['amount'] = (float) $bet['bet'] * 6;
+		$_SESSION['amount'] = ((float) $bet['amount'] * (int) $config['multiply']);
 		$_SESSION['lose']++;
 		sleep($_SESSION['lose']);
 	}
